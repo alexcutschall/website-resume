a8613ebf1ac0657c3266ba9fd0434d74ff7d07b9
alexcutschall <alexander.cutschall@gmail.com>
2021-08-13 13:27:06 -0600

    Cleaning up styling even more

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  title = 'Alex Cutschall';
+  subtitle = 'Programmer | Learner | Website Tinkerer'
   constructor() { }
 
   ngOnInit() {
