103d2dbcb7f35b40ceca9c3a6068a95786cec1f5
alexcutschall <alexander.cutschall@gmail.com>
2021-08-16 11:22:51 -0600

    Cleaning up unused code

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -5,21 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent implements OnInit {
+export class ResumeComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-  downloadMyFile(){
-    // const link = document.createElement('a');
-    // link.setAttribute('target', '_blank');
-    // link.setAttribute('href', './../../assets/alex-cutschall-resume.pdf');
-    // link.setAttribute('download', `alex-cutschall-resume.pdf`);
-    // document.body.appendChild(link);
-    // link.click();
-    // link.remove();
-  }
 
 }
