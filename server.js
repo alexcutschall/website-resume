cbe847a08da8b394b2a7f8a495a8547eb29f1939
alexcutschall <alexander.cutschall@gmail.com>
2021-08-17 11:12:01 -0600

    Working local example with express

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/alex-website-resume'));
+app.use(requireHTTPS);
+app.use(express.static('./dist/alex-website-resume'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/alex-website-resume/index.html'));
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/alex-website-resume'}),
+);
+
+app.listen(8080, () => {
+  console.log('ITS LISTENING');
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
