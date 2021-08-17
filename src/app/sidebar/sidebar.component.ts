c9a965452b1883967c4379665a1813352a3a1fe0
alexcutschall <alexander.cutschall@gmail.com>
2021-08-17 10:24:00 -0600

    Fixing url and favicon

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -25,8 +25,4 @@ export class SidebarComponent implements OnInit {
   navigate(path: string) {
     this.router.navigate([ path ]);
   }
-
-  contactImageClicked(type) {
-    console.log('IMAGE WAS CLICKED', type)
-  }
 }
